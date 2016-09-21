15faaae79b350ad14b0f643e2f7b1e85e1db8092
zhougit86 <zhou16386@163.com>
2016-09-21 09:50:59 +0800

    add

---
+++ stevedoretest/my_driver.py
@@ -0,0 +1,9 @@
+class SimpleDriver1:
+
+    def get_name(self):
+        return "This is SimpleDriver1"
+
+class SimpleDriver2:
+
+    def get_name(self):
+        return "This is SimpleDriver2"
---
+++ stevedoretest/my_extension.py
@@ -0,0 +1,9 @@
+class SimpleExtension1:
+
+    def get_name(self):
+        return "This is SimpleExtension1"
+
+class SimpleExtension2:
+
+    def get_name(self):
+        return "This is SimpleExtension2"
